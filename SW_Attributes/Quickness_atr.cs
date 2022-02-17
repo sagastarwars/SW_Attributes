1cfeae948402ab9b690d816d5d3c23e9c16d3d59
AVazianskii <avazianskii@mail.ru>
2022-02-17 16:44:41 +0300

    changed hardcode configuration fields

--- SW_Attributes/Quickness_atr.cs
+++ SW_Attributes/Quickness_atr.cs
@@ -8,24 +8,7 @@ namespace Attribute_libs
         public Quickness_atr() // определяем конструктор класса, используя конструктор базового класса
         {
             Set_atr_score(0);                       // Устанавливаем текущее показатель атрибута 
-            // Set_description("");                    // Устанавливаем описание атрибута
-            //Set_atribute_code((int)SW_Character.enum_Atributes.Quicness);    // Устанавливаем код текущего атрибута
-            
-
-
-            Set_child_attribute_limit(2);           // Устанавливаем значение возрастного лимита атрибутов для Ребенок
-            Set_teen_attribute_limit(4);            // Устанавливаем значение возрастного лимита атрибутов для Подросток
-            Set_adult_attribute_limit(10);          // Устанавливаем значение возрастного лимита атрибутов для Взрослый
-            Set_middle_attribute_limit(10);         // Устанавливаем значение возрастного лимита атрибутов для Средний Возраст
-            Set_old_attribute_limit(10);            // Устанавливаем значение возрастного лимита атрибутов для Пожилой
-            Set_eldery_attribute_limit(10);         // Устанавливаем значение возрастного лимита атрибутов для Почтенный
-            Set_unknown_age_attribute_limit(0);     // Устанавливаем значение возрастного лимита атрибутов для Неизвестный возраст
-
-            Set_private_range_attribute_limit(10);  // Устанавливаем значение рангового лимита атрибутов для Рядовой 
-            Set_veteran_range_attribute_limit(10);  // Устанавливаем значение рангового лимита атрибутов для Ветеран 
-            Set_hero_range_attribute_limit(10);     // Устанавливаем значение рангового лимита атрибутов для Герой 
-            Set_epic_range_attribute_limit(10);     // Устанавливаем значение рангового лимита атрибутов для Эпик 
-            Set_immortal_range_attribute_limit(10); // Устанавливаем значение рангового лимита атрибутов для Бессмертный
+            Set_atribute_code((int)Attribute_manager.enum_Atributes.Quicness);    // Устанавливаем код текущего атрибута
         }
         #endregion
 
